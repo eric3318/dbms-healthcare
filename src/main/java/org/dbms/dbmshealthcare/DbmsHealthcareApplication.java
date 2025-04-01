6c1f1398567f0a348110038deb4acb607555c107
Han Nie <nie.han@northeastern.edu>
2025-03-31 20:44:35 -0700

    Initial commit

---
+++ src/main/java/org/dbms/dbmshealthcare/DbmsHealthcareApplication.java
@@ -0,0 +1,13 @@
+package org.dbms.dbmshealthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsHealthcareApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DbmsHealthcareApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/dbms/dbmshealthcare/DbmsHealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dbms.dbmshealthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsHealthcareApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
