89ec445f4deb355d1bc87de41e81e28adf0cb0ec
raulitoflying <1017329098@qq.com>
2025-04-03 17:46:11 -0700

    Refactor PatientRepository to implement MongoDB operations using MongoTemplate instead of an interface, enhancing query capabilities for patient data retrieval.

--- src/main/java/org/dbms/dbmshealthcare/repository/PatientRepository.java
+++ src/main/java/org/dbms/dbmshealthcare/repository/PatientRepository.java
@@ -2,16 +2,30 @@ package org.dbms.dbmshealthcare.repository;
 
 import java.util.List;
 import org.dbms.dbmshealthcare.model.Patient;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PatientRepository extends BaseMongoRepository<Patient, String> {
-    
-    List<Patient> findByUserId(String userId);
-    
-    List<Patient> findByDoctorId(String doctorId);
-    
-    @Query("{'personalHealthNumber': ?0}")
-    Patient findByPersonalHealthNumber(String personalHealthNumber);
-} 
+public class PatientRepository extends BaseMongoRepository<Patient> {
+
+    public PatientRepository(MongoTemplate template) {
+        super(template, Patient.class);
+    }
+
+    public List<Patient> findByUserId(String userId) {
+        Query query = new Query(Criteria.where("userId").is(userId));
+        return mongoTemplate.find(query, Patient.class);
+    }
+
+    public List<Patient> findByDoctorId(String doctorId) {
+        Query query = new Query(Criteria.where("doctorId").is(doctorId));
+        return mongoTemplate.find(query, Patient.class);
+    }
+
+    public Patient findByPersonalHealthNumber(String personalHealthNumber) {
+        Query query = new Query(Criteria.where("personalHealthNumber").is(personalHealthNumber));
+        return mongoTemplate.findOne(query, Patient.class);
+    }
+}
