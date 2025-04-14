8c40ddc5baba98f1ab9716e01b91fae7ffd0015f
“MeilinNiu” <niumeilingrace@gmail.com>
2025-04-13 18:21:45 -0700

    fix medical dto

--- src/main/java/org/dbms/dbmshealthcare/dto/MedicalRecordUpdateDto.java
+++ src/main/java/org/dbms/dbmshealthcare/dto/MedicalRecordUpdateDto.java
@@ -1,12 +1,13 @@
 package org.dbms.dbmshealthcare.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import java.math.BigDecimal;
 import java.util.List;
 import org.dbms.dbmshealthcare.model.pojo.Prescription;
 
-public record MedicalRecordUpdateDto(String patientDescription, String doctorNotes,
-                                     String finalDiagnosis,
-                                     List<String> requisitionIds, List<Prescription> prescriptions,
-                                     BigDecimal billingAmount) {
-
-}
+public record MedicalRecordUpdateDto(
+    String doctorNotes,
+    String finalDiagnosis,
+    List<Prescription> prescriptions,
+    BigDecimal billingAmount
+) {} 
